e5e4af486eb15818fa70a8da25090af24b1b3973
sl <bj_ling@sina.com>
2017-03-01 20:16:54 +0800

    Update HippoApigateApplication.java

--- src/main/java/com/github/HippoApigateApplication.java
+++ src/main/java/com/github/HippoApigateApplication.java
@@ -2,8 +2,10 @@ package com.github;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @MapperScan("com.github.hippo.mapper")
 public class HippoApigateApplication {
